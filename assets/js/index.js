4e035c6ddafce5355f699481c61813e617c7d024
kamlesh914213 <143068135+kamlesh914213@users.noreply.github.com>
2025-07-23 03:55:45 +0530

    Update footer with social icons

--- assets/js/index.js
+++ assets/js/index.js
@@ -96,4 +96,19 @@ $(document).ready(function () {
 });
 
 
+ $(document).ready(function(){
+    $(".gallery-carousel").owlCarousel({
+      loop: true,
+      margin: 20,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: { items: 1 },
+        576: { items: 1 },
+        768: { items: 2 },
+        992: { items: 4 }
+      }
+    });
+  });
+
 
